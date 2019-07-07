2ed3d3d911ac26cefac3567fa75cc2faa47bebeb
unknown <clonebmn2itt@gmail.com>
2019-07-07 21:08:15 +0700

    * Beautify appstate

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { email, password, appStateFile } = require("./config/");
 const fs = require("fs");
 facebook = ({ Op, models }) => login({ email, password, appState: require(appStateFile) }, function (error, api) {
     if (error) return logger(error, 2);
-    fs.writeFileSync(appStateFile, JSON.stringify(api.getAppState()));
+    fs.writeFileSync(appStateFile, JSON.stringify(api.getAppState(), null, '\t'));
     logger('Đăng nhập thành công!', 0);
     //Listening
     api.listen(require("./app/listen")({ api, Op, models }))
