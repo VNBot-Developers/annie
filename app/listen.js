4178e8eb3afaa54e8a3be9ca319d0bd65fed12b8
unknown <clonebmn2itt@gmail.com>
2019-07-08 00:49:51 +0700

    Add module-alias

--- app/listen.js
+++ app/listen.js
@@ -1,34 +1,40 @@
 const modules = require("./modules");
 const config = require("../config");
 const __GLOBAL = new Object({
-    threadBlocked : new Array(),
+    threadBlocked: new Array(),
     userBlocked: new Array(),
     swearList: new Array()
 });
-module.exports = function({ api, models }) {
+module.exports = function ({ api, models }) {
     let UserModel = models.use('user');
     let ThreadModel = models.use('thread');
-    ThreadModel.findAll({ where: { block: true }})
-    .then(e => {
-        __GLOBAL.threadBlocked = e.map(e => e.get({
-            plain: true
-        }))
-    });
+    ThreadModel.findAll({ where: { block: true } })
+        .then(e => {
+            __GLOBAL.threadBlocked = e.map(e => e.get({
+                plain: true
+            }))
+        });
 
-    UserModel.findAll({ where: { block: true }})
-    .then(e => {
-        __GLOBAL.userBlocked = e.map(e => e.get({
-            plain: true
-        }))
-    })
+    UserModel.findAll({ where: { block: true } })
+        .then(e => {
+            __GLOBAL.userBlocked = e.map(e => e.get({
+                plain: true
+            }))
+        })
     const handleMessage = require("./handle/message")({ api, modules, config, __GLOBAL });
     const handleEvent = require("./handle/event")({ api, modules, config, __GLOBAL });
     const handleMessageReaction = require("./handle/message_reaction")({ api, modules, config, __GLOBAL });
     modules.log(config.prefix || '<none>', '[ prefix ]');
     modules.log(`${api.getCurrentUserID()} - ${config.botName}`, '[ UID ]');
     modules.log('Bắt đầu listen!');
-    return function(error, event) {
-        if(error) return modules.log(error , 2);
+    return function (error, event) {
+        if (error) return modules.log(error, 2);
+        console.log(event);
+        api.getUserInfo(event.senderID, (err, ret) => {
+            if (err) return console.error(err);
+
+            console.log(ret)
+        })
         switch (event.type) {
             case 'message':
                 handleMessage({ event })
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
-const login = require("./app/login");
-const { Sequelize, sequelize, Op } = require("./database/index");
-const logger = require("./app/modules/log");
-const { email, password, appStateFile } = require("./config/");
+const login = require("@app/login");
+const { Sequelize, sequelize, Op } = require("@database");
+const logger = require("@logger");
+const { email, password, appStateFile } = require("@config");
 const fs = require("fs");
 facebook = ({ Op, models }) => login({ email, password, appState: require(appStateFile) }, function (error, api) {
     if (error) return logger(error, 2);
     fs.writeFileSync(appStateFile, JSON.stringify(api.getAppState(), null, '\t'));
     logger('Đăng nhập thành công!', 0);
     //Listening
-    api.listen(require("./app/listen")({ api, Op, models }))
+    api.listen(require("@app/listen")({ api, Op, models }))
 })
 sequelize.authenticate()
     .then(() => logger('Connect database thành công!', 0))
     .then(() => {
-        let models = require("./database/model")({ Sequelize, sequelize });
+        let models = require("@database/model")({ Sequelize, sequelize });
         facebook({ Op, models });
     })
     .catch((e) => {
